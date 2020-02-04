ba13e2494ea52493fc8d2697dcb9bb1a9b726074
Karim Abdel-Qader <karim-abdelqader@localhost.localdomain>
2020-02-04 10:15:58 +0200

    finished crud operations for todo frontend

--- Front-end/src/app/todo/todo.component.ts
+++ Front-end/src/app/todo/todo.component.ts
@@ -3,38 +3,41 @@ import {Todo} from '../ClassTodo';
 import {TodoDataService} from '../todo-data.service';
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+    selector: 'app-todo',
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
 
-  constructor(private todoService: TodoDataService) {
-  }
-
-  todos: Todo[];
-
-  ngOnInit() {
-    this.getTodos();
-  }
-
-  getTodos(): void {
-    this.todoService.getTodos().subscribe(todos => this.todos = todos);
-  }
-
-  add(name: string): void {
-    name = name.trim();
-    if (!name) {
-      return;
+    constructor(private todoService: TodoDataService) {
     }
-    this.todoService.addTodo({name} as Todo).subscribe(todo => {
-      this.todos.push(todo);
-    });
-  }
 
-  delete(todo: Todo): void {
-    this.todos = this.todos.filter(t => t !== todo);
-    this.todoService.deleteTodo(todo).subscribe();
-  }
+    todos: Todo[];
+
+    ngOnInit() {
+        this.getTodos();
+    }
+
+    getTodos(): void {
+        this.todoService.getTodos().subscribe(todos => this.todos = todos);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) {
+            return;
+        }
+        this.todoService.addTodo({name} as Todo).subscribe(todo => {
+            if (todo.name) {
+                this.todos.push(todo);
+            }
+
+        });
+    }
+
+    delete(todo: Todo): void {
+        this.todos = this.todos.filter(t => t !== todo);
+        this.todoService.deleteTodo(todo).subscribe();
+    }
 
 }
