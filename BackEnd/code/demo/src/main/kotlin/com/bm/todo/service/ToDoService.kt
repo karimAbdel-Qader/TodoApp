f06c8e3845ebb7211d65685cedb92454121c9178
shady.elkady <shady.elkady@brightskiesinc.com>
2020-02-04 13:38:13 +0200

    add docs

--- BackEnd/code/demo/src/main/kotlin/com/bm/todo/service/ToDoService.kt
+++ BackEnd/code/demo/src/main/kotlin/com/bm/todo/service/ToDoService.kt
@@ -6,15 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
+/**
+ *
+ * This class has handling all the logic for the Todos.
+ *
+ * @param toDoRepository repository for Todos.
+ * @constructor Creates a ToDoService with repository.
+ */
 @Service
 class ToDoService @Autowired constructor(
         val toDoRepository: ToDoRepository) {
 
 
+    /**
+     * Fetch all unfinished ToDos
+     */
     fun getUnfinishedTodos() : List<TODO> = toDoRepository.findByStatus(false)
 
+    /**
+     * Creates new Tod
+     *
+     * @param todo
+     */
     fun addTodo(todo: TODO) = toDoRepository.save(todo)
 
+    /**
+     * Update specific Tod by it's id
+     *
+     * @param todoId
+     */
     @Transactional
     fun updateToDoStatus(todoId: Int) {
         var todo = toDoRepository.findById(todoId).get()
