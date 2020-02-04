c93e6b68381ff354fe4e5b5dafd6cbfa72f6eda0
Karim Abdel-Qader <karim-abdelqader@localhost.localdomain>
2020-02-04 10:02:07 +0200

    temporary commit

--- Front-end/src/app/todo-data.service.ts
+++ Front-end/src/app/todo-data.service.ts
@@ -5,71 +5,72 @@ import {catchError, tap} from 'rxjs/operators';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {MessageService} from './message.service';
 
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TodoDataService {
-  private apiUrl = 'api/v1/Todos';
-  httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-  };
+    private apiUrl = 'api/v1/Todos';
+    httpOptions = {
+        headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
 
-  constructor(private message: MessageService, private http: HttpClient) {
-  }
+    constructor(private message: MessageService, private http: HttpClient) {
+    }
 
-  private log(message: string) {
-    this.message.add(`HeroService: ${message}`);
-  }
+    private log(message: string) {
+        this.message.add(`HeroService: ${message}`);
+    }
 
 // GET ALL TODOO
-  getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.apiUrl)
-      .pipe(
-        tap(_ => this.log('fetched todos')),
-        catchError(this.handleError('getTodos', []))
-      );
-  }
+    getTodos(): Observable<Todo[]> {
+        return this.http.get<Todo[]>(this.apiUrl)
+            .pipe(
+                tap(_ => this.log('fetched todos')),
+                catchError(this.handleError('getTodos', []))
+            );
+    }
 
-  // ADD A TODOO
-  addTodo(todo: Todo): Observable<Todo> {
+    // ADD A TODOO
+    addTodo(todo: Todo): Observable<Todo> {
 
-    return this.http.post<Todo>(this.apiUrl, todo, this.httpOptions).pipe(
-      tap(_ => this.log(`added todo w/ id=${todo.id}`)),
-      catchError(this.handleError<Todo>('addTodo'))
-    );
-  }
+        return this.http.post<Todo>(this.apiUrl, todo, this.httpOptions).pipe(
+            tap(_ => this.log(`added todo w/ id=${todo.id}`)),
+            catchError(this.handleError<Todo>('addTodo'))
+        );
+    }
 
-  // DELETE A TODOO
-  deleteTodo(todo: Todo | number): Observable<Todo> {
-    const id = typeof todo === 'number' ? todo : todo.id;
-    const url = `${this.apiUrl}/${id}`;
-    return this.http.delete<Todo>(url, this.httpOptions).pipe(
-      tap(_ => this.log(`deleted todo id=${id}`)),
-      catchError(this.handleError<Todo>('deleteTodo'))
-    );
-  }
+    // DELETE A TODOO
+    deleteTodo(todo: Todo | number): Observable<Todo> {
+        const id = typeof todo === 'number' ? todo : todo.id;
+        const url = `${this.apiUrl}/${id}`;
+        return this.http.delete<Todo>(url, this.httpOptions).pipe(
+            tap(_ => this.log(`deleted todo id=${id}`)),
+            catchError(this.handleError<Todo>('deleteTodo'))
+        );
+    }
 
-  // UPDATE A TODOO
+    // UPDATE A TODOO
 
-  UpdateTodo(todo: Todo): Observable<any> {
-    return this.http.put(this.apiUrl, this.httpOptions).pipe(
-      tap(_ => this.log(`updated todo id=${todo.id}`)),
-      catchError(this.handleError<Todo>('updateTodo'))
-    );
+    UpdateTodo(todo: Todo): Observable<any> {
+        return this.http.put(this.apiUrl, this.httpOptions).pipe(
+            tap(_ => this.log(`updated todo id=${todo.id}`)),
+            catchError(this.handleError<Todo>('updateTodo'))
+        );
 
-  }
+    }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+            // TODO: better job of transforming error for user consumption
+            this.log(`${operation} failed: ${error.message}`);
 
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
 }
